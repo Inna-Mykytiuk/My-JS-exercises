4da0345e7e3dbb1ed22d0814dd24f99c92b16113
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-03-06 23:38:47 +0200

    mod-3 objects

--- src/js/mod-3.js
+++ src/js/mod-3.js
@@ -1,3 +1,86 @@
+//Якщо значення властивості - це функція, така властивість називається методом об'єкта.
+// ✅ Логічно і синтаксично згруповані сутності
+// const bookShelf = {
+//   books: ['The Last Kingdom', 'Dream Guardian'],
+//   // Це метод об'єкта
+//   getBooks() {
+//     console.log('Цей метод буде повертати всі книги - властивість books');
+//   },
+//   // Це метод об'єкта
+//   addBook(bookName) {
+//     console.log('Цей метод буде додавати нову книгу у властивість books');
+//   },
+// };
+
+/*Методи використовуються для роботи з властивостями об'єкта, їх зміни. Для доступу до об'єкта в методі використовується не ім'я змінної, наприклад bookShelf, а ключове слово this - контекст. Значенням this буде об'єкт перед «крапкою», тобто об'єкт, який викликав цей метод, у нашому випадку - це посилання на об'єкт bookShelf.*/
+// const bookShelf = {
+//   books: ["The Last Kingdom"],
+//   getBooks() {
+//     console.log(this);
+//   },
+// };
+
+// // Перед крапкою знаходиться об'єкт bookShelf,
+// // тому, викликаючи метод, this буде зберігати посилання на нього.
+// bookShelf.getBooks(); // {books: ['The Last Kingdom'], getBooks: f}
+// // Виклики методів
+// bookShelf.getBooks();
+// bookShelf.addBook('Нова книга');
+
+/*Для того, щоб отримати доступ до властивостей об'єкта в методах, ми звертаємось до нього через this і далі, стандартно - «через крапку» до властивостей. */
+// const bookShelf = {
+//   books: ['The Last Kingdom'],
+//   getBooks() {
+//     return this.books;
+//   },
+//   addBook(bookName) {
+//     this.books.push(bookName);
+//   },
+//   removeBook(bookName) {
+//     const bookIndex = this.books.indexOf(bookName);
+//     this.books.splice(bookIndex, 1);
+//   },
+// };
+
+// console.log(bookShelf.getBooks()); // ["The Last Kingdom"]
+// bookShelf.addBook('The Mist');
+// bookShelf.addBook('Dream Guardian');
+// console.log(bookShelf.getBooks()); // ['The Last Kingdom', 'The Mist', 'Dream Guardian']
+// bookShelf.removeBook('The Mist');
+// console.log(bookShelf.getBooks()); // ['The Last Kingdom', 'Dream Guardian']
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Ключ
+//   console.log(key);
+//   // Значення властивості з таким ключем
+//   console.log(book[key]);
+// }
+
+//--------------hasOwnProperty
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Якщо це власна властивість - виконуємо тіло if
+//   if (book.hasOwnProperty(key)) {
+//     console.log(key);
+//     console.log(book[key]);
+//   }
+
+//   // Якщо це невласна властивість - нічого не робимо
+// }
+
 // # Модуль 3. Занятие 1. Объекты
 
 // ## Example 1 - Основы обьектов
@@ -20,11 +103,16 @@
 // };
 
 // user.mood = 'happy';
-// user.hobby = '';
+// user.hobby = 'skydiving';
 // user.premium = false;
 
-// for (let us of user) {
-//   console.log(Object.keys(us));
+// //Отримуємо ключі через Object.keys
+// const keys = Object.keys(user);
+// console.log('keys', keys);
+
+// //Отримуємо значення ключів через user[key]
+// for (let key of keys) {
+//   console.log(`${key}: ${user[key]}`);
 // }
 
 // ## Example 2 - метод Object.values()
@@ -41,7 +129,37 @@
 //   Ann: 160,
 //   Pete: 130,
 // };
-// ```
+// // ```
+// //v1
+// //   let sum = 0;
+
+// //   const values = Object.values(salaries);
+// //   for (let value of values) {
+// //     sum += value;
+// // }
+
+// // console.log(sum);
+
+// //v2
+// const salaries1 = {};
+
+// const calcSalaries = function (salaries) {
+//   //Виводимо значення ключів
+//   const values = Object.values(salaries);
+//   //Якщо обєкт порожній - виводимо 0
+//   if (values.length === 0) {
+//     return 0;
+//   }
+
+//   let sum = 0;
+//   for (let value of values) {
+//     sum += value;
+//   }
+//   return sum;
+// };
+
+// console.log(calcSalaries(salaries1));
+// console.log(calcSalaries(salaries));
 
 // ## Example 3 - Массив объектов
 
@@ -52,13 +170,29 @@
 // ### Код
 
 // ```js
-// const stones = [
-//   { name: 'Изумруд', price: 1300, quantity: 4 },
-//   { name: 'Бриллиант', price: 2700, quantity: 3 },
-//   { name: 'Сапфир', price: 400, quantity: 7 },
-//   { name: 'Щебень', price: 200, quantity: 2 },
-// ];
+const stones = [
+  { name: 'Изумруд', price: 1300, quantity: 4 },
+  { name: 'Бриллиант', price: 2700, quantity: 3 },
+  { name: 'Сапфир', price: 400, quantity: 7 },
+  { name: 'Щебень', price: 200, quantity: 2 },
+];
 // ```
+
+function calcTotalPrice(stones, stoneName) {
+  //обраний камінь невідомий
+  let targetstone = null;
+  //для кожного камня із колецкції робимо порівняння на збіг по імені
+  for (let stone of stones) {
+    if (stone.name === stoneName) {
+      // якщо в нас є такий камінь записуємо його в змінну
+      targetstone = stone;
+    }
+  }
+  //повертаємо загальну вартість каменю
+  return targetstone.price * targetstone.quantity;
+}
+
+console.log(calcTotalPrice(stones, 'Сапфир'));
 
 // ## Example 4 - Комплексные задачи
 
