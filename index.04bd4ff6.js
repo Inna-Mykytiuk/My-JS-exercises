a2ae29149af7cdf6f692c7fe1f2648464497cb3e
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-03-06 21:39:19 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/My-JS-exercises@4da0345e7e3dbb1ed22d0814dd24f99c92b16113 🚀

---
+++ index.04bd4ff6.js
@@ -0,0 +1,2 @@
+console.log(function(n,e){let t=null;for(let i of n)i.name===e&&(t=i);return t.price*t.quantity}([{name:"Изумруд",price:1300,quantity:4},{name:"Бриллиант",price:2700,quantity:3},{name:"Сапфир",price:400,quantity:7},{name:"Щебень",price:200,quantity:2}],"Сапфир"));
+//# sourceMappingURL=index.04bd4ff6.js.map
--- index.3dd697ae.js
+++
@@ -1,2 +0,0 @@
-!function(){const n=function(...n){if(0===n.length)return 0;let t=0;for(let e=0;e<n.length;e++)t+=n[e];return t/n.length}(2,5,8,11);console.log(n)}();
-//# sourceMappingURL=index.3dd697ae.js.map
