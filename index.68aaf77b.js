ed4091682c9064a51aff55aa139660487bd33b72
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-03-07 13:49:19 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/My-JS-exercises@64386866a592688e34088eda734f66010874cefe 🚀

---
+++ index.68aaf77b.js
@@ -0,0 +1,2 @@
+!function(){let o={name:"Inna",param1:15},n={...o},l={...o,name:"Inna",param2:15};console.log(n),console.log(l);let e={...o,bike:!0};function a(o,...n){console.log("a",o),console.log("b",n)}console.log("b3",e),a(1,2),a(1,2,3,4,5,6,7);let[c,...s]=[100,200,300,400];console.log("s11",s)}();
+//# sourceMappingURL=index.68aaf77b.js.map
---
+++ index.c8fee8bf.js
@@ -0,0 +1,2 @@
+let o={name:"Inna",param1:15},l={...o},n={...o,name:"Inna",param2:15};console.log(l),console.log(n);let e={...o,bike:!0};function a(o,...l){console.log("a",o),console.log("b",l)}console.log("b3",e),a(1,2),a(1,2,3,4,5,6,7);let[c,...s]=[100,200,300,400];console.log("s11",s);
+//# sourceMappingURL=index.c8fee8bf.js.map
