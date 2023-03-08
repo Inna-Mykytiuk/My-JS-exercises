ce5705106b45e7d77dcf4994dc3fde8430ff7248
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-03-08 13:14:38 +0200

    array iterative methods

--- src/js/mod-4-part-2.js
+++ src/js/mod-4-part-2.js
@@ -87,88 +87,88 @@
 // ## Коллекция объектов для всех примеров с автомобилями
 
 // ```js
-// const cars = [
-//   {
-//     make: 'Honda',
-//     model: 'CR-V',
-//     type: 'suv',
-//     amount: 14,
-//     price: 24045,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Honda',
-//     model: 'Accord',
-//     type: 'sedan',
-//     amount: 2,
-//     price: 22455,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Mazda',
-//     model: 'Mazda 6',
-//     type: 'sedan',
-//     amount: 8,
-//     price: 24195,
-//     onSale: false,
-//   },
-//   {
-//     make: 'Mazda',
-//     model: 'CX-9',
-//     type: 'suv',
-//     amount: 7,
-//     price: 31520,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Toyota',
-//     model: '4Runner',
-//     type: 'suv',
-//     amount: 19,
-//     price: 34210,
-//     onSale: false,
-//   },
-//   {
-//     make: 'Toyota',
-//     model: 'Sequoia',
-//     type: 'suv',
-//     amount: 16,
-//     price: 45560,
-//     onSale: false,
-//   },
-//   {
-//     make: 'Toyota',
-//     model: 'Tacoma',
-//     type: 'truck',
-//     amount: 4,
-//     price: 24320,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Ford',
-//     model: 'F-150',
-//     type: 'truck',
-//     amount: 11,
-//     price: 27110,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Ford',
-//     model: 'Fusion',
-//     type: 'sedan',
-//     amount: 13,
-//     price: 22120,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Ford',
-//     model: 'Explorer',
-//     type: 'suv',
-//     amount: 6,
-//     price: 31660,
-//     onSale: false,
-//   },
-// ];
+const cars = [
+  {
+    make: 'Honda',
+    model: 'CR-V',
+    type: 'suv',
+    amount: 14,
+    price: 24045,
+    onSale: true,
+  },
+  {
+    make: 'Honda',
+    model: 'Accord',
+    type: 'sedan',
+    amount: 2,
+    price: 22455,
+    onSale: true,
+  },
+  {
+    make: 'Mazda',
+    model: 'Mazda 6',
+    type: 'sedan',
+    amount: 8,
+    price: 24195,
+    onSale: false,
+  },
+  {
+    make: 'Mazda',
+    model: 'CX-9',
+    type: 'suv',
+    amount: 7,
+    price: 31520,
+    onSale: true,
+  },
+  {
+    make: 'Toyota',
+    model: '4Runner',
+    type: 'suv',
+    amount: 19,
+    price: 34210,
+    onSale: false,
+  },
+  {
+    make: 'Toyota',
+    model: 'Sequoia',
+    type: 'suv',
+    amount: 16,
+    price: 45560,
+    onSale: false,
+  },
+  {
+    make: 'Toyota',
+    model: 'Tacoma',
+    type: 'truck',
+    amount: 4,
+    price: 24320,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'F-150',
+    type: 'truck',
+    amount: 11,
+    price: 27110,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'Fusion',
+    type: 'sedan',
+    amount: 13,
+    price: 22120,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'Explorer',
+    type: 'suv',
+    amount: 6,
+    price: 31660,
+    onSale: false,
+  },
+];
 
 // const models = cars.map(car => car.model);
 // console.log('models', models);
@@ -190,7 +190,7 @@
 //   return cars.map(({ model }) => model);
 // }
 
-// const getModels = (cars) => cars.map(({ model }) => model);
+// const getModels = cars => cars.map(({ model }) => model);
 
 // 1) car = {
 //   make: "Honda",
@@ -231,7 +231,7 @@
 // }
 
 // const makeCarsWithDiscount = (cars, discount) =>
-//   cars.map((car) => ({ ...car, price: car.price * (1 - discount) }));
+//   cars.map(car => ({ ...car, price: car.price * (1 - discount) }));
 
 // const discount20 = makeCarsWithDiscount(cars, 0.2);
 
@@ -258,6 +258,9 @@
 // Пусть функция `getCarsWithDiscount` возвращает массив автомобилей свойство
 // onSale которых true.
 
+// const getCarsWithDiscount = cars => cars.filter(car => car.onSale === true);
+// console.table(getCarsWithDiscount(cars));
+// console.table('cars', cars);
 // ```js
 // const getCarsWithDiscount = (cars) =>
 //   cars.filter(({ onSale }) => onSale === true);
