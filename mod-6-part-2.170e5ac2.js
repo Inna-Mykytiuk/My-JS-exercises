772b99bbc29c1fac8c650b0d9e8aa5da153e1897
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-03-09 16:02:36 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/My-JS-exercises@a2e68429e1ae264a6b9d9a068bfcfb102d23229f 🚀

---
+++ mod-6-part-2.170e5ac2.js
@@ -0,0 +1,2 @@
+!function(){const o=document.querySelector("#categories");console.log("list",o)}();
+//# sourceMappingURL=mod-6-part-2.170e5ac2.js.map
---
+++ mod-6-part-2.8682e2ea.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#categories");console.log("list",e);
+//# sourceMappingURL=mod-6-part-2.8682e2ea.js.map
