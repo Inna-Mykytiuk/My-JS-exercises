2271ffc28650b1e25065be5980e9b2348c3d7026
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-03-09 17:33:17 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/My-JS-exercises@257c7d077993ab7c98d4b0cfd9b3f0def6be0313 🚀

--- mod-6-part-2.367fed56.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".overlay"),t=document.querySelector(".close"),n=document.querySelector(".open"),d=()=>{e.classList.toggle("hide")};addEventListener("DOMContentLoaded",d),window.addEventListener("DOMContentLoaded",d),t.addEventListener("click",d),n.addEventListener("click",d);
-//# sourceMappingURL=mod-6-part-2.367fed56.js.map
--- mod-6-part-2.8b5874ce.js
+++
@@ -1,2 +0,0 @@
-!function(){const e=document.querySelector(".overlay"),t=document.querySelector(".close"),n=document.querySelector(".open"),d=()=>{e.classList.toggle("hide")};addEventListener("DOMContentLoaded",d),window.addEventListener("DOMContentLoaded",d),t.addEventListener("click",d),n.addEventListener("click",d)}();
-//# sourceMappingURL=mod-6-part-2.8b5874ce.js.map
