78c2f1479eb28d95af8aca06b07fd3dc56324d8a
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-03-11 19:34:28 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/My-JS-exercises@938399c538fa3931823a734f0edb0fe944a8a2a3 🚀

--- mod-7.b47fe468.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=mod-7.b47fe468.js.map
---
+++ mod-8.53c75b85.js
@@ -0,0 +1,2 @@
+const iframe=document.querySelector("#vimeo-player"),player=new Vimeo.Player(iframe);console.log(player);
+//# sourceMappingURL=mod-8.53c75b85.js.map
