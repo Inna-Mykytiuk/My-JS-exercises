718ea0e0498cf7fd705e073f1ef88a567a2ecf92
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-03-12 14:13:48 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/My-JS-exercises@c92906aa1caff66347957844652d430385792f40 🚀

--- mod-9.c1269851.js
+++
@@ -1,2 +0,0 @@
-const isSuccess=!1,promise=new Promise(((e,o)=>{setTimeout((()=>{o("Error")}),2e3)}));console.log("before promise .then"),promise.then((e=>{console.log("On resolve inside promise"),console.log("value",e)}),(e=>{console.log("On reject inside promise"),console.log("error",e)}));
-//# sourceMappingURL=mod-9.c1269851.js.map
---
+++ mod-9.fc3d0190.js
@@ -0,0 +1,2 @@
+const result=new Promise(((e,l)=>{let o=0;for(let e=0;e<1e6;e++)o+=e;o%2==0?e(o):l(8)}));console.log("result",result);
+//# sourceMappingURL=mod-9.fc3d0190.js.map
